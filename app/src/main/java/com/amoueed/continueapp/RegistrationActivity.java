090ead996756ed9736bd73b39aee5a3d63309398
Rao Moueed Ahmed <moueedrao@gmail.com>
2019-03-21 16:53:41 +0500

    Add: FirebaseAuth user creation in RegistrationActivity

--- app/src/main/java/com/amoueed/continueapp/RegistrationActivity.java
+++ app/src/main/java/com/amoueed/continueapp/RegistrationActivity.java
@@ -1,15 +1,32 @@
 package com.amoueed.continueapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.button.MaterialButton;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
-    MaterialButton cancelButtonRegistration;
-    TextView registrationTextViewRegistration;
+    private MaterialButton cancelButtonRegistration;
+    private TextView registrationTextViewRegistration;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    private TextInputEditText numberEditTextRegistration;
+    private TextInputEditText passwordEditRegistration;
+    private MaterialButton signUpButtonRegistration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +45,44 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View view) {
                 finish();
         }
+        });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        numberEditTextRegistration = findViewById(R.id.numberEditTextRegistration);
+        passwordEditRegistration = findViewById(R.id.passwordEditRegistration);
+        signUpButtonRegistration = findViewById(R.id.signUpButtonRegistration);
+        signUpButtonRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mEmail = numberEditTextRegistration.getText().toString().trim();
+                String mPassword = passwordEditRegistration.getText().toString().trim();
+                if(TextUtils.isEmpty(mEmail)){
+                    numberEditTextRegistration.setError("Required!");
+                    return;
+                }
+                if(TextUtils.isEmpty(mPassword)){
+                    passwordEditRegistration.setError("Required!");
+                    return;
+                }
+                progressDialog.setMessage("Processing...");
+                progressDialog.show();
+
+                firebaseAuth.createUserWithEmailAndPassword(mEmail,mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"Sucessfull",Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(),ChildReadActivity.class));
+                            progressDialog.dismiss();
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(),"Problem in Auth",Toast.LENGTH_LONG).show();
+                            progressDialog.dismiss();
+                        }
+                    }
+                });
+            }
         });
     }
 }
