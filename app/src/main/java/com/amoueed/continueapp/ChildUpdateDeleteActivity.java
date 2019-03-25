4a8ec1adf9be31688f706b16a3be1c2811721941
Rao Moueed Ahmed <moueedrao@gmail.com>
2019-03-25 08:23:06 +0500

    Add: update functionality in ChildUpdateDeleteActivity.java

--- app/src/main/java/com/amoueed/continueapp/ChildUpdateDeleteActivity.java
+++ app/src/main/java/com/amoueed/continueapp/ChildUpdateDeleteActivity.java
@@ -1,15 +1,26 @@
 package com.amoueed.continueapp;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.design.button.MaterialButton;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.amoueed.continueapp.Model.Child;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
 
 public class ChildUpdateDeleteActivity extends AppCompatActivity {
     private MaterialButton updateButton;
@@ -21,14 +32,27 @@ public class ChildUpdateDeleteActivity extends AppCompatActivity {
     private TextInputEditText phone;
     private TextInputEditText email;
 
+    static final int DOB_DIALOG_ID = 900;
+    private int year;
+    private int month;
+    private int day;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_update_delete);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        String uId = user.getUid();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Child").child(uId);
+
         Intent i = getIntent();
         Bundle b = i.getExtras();
         Child c = b.getParcelable("myChild");
+        final String mId = c.getId();
         String mName = c.getChildName();
         String mDateOfBirth = c.getDateOfBirth();
         String mGender = c.getGender();
@@ -54,6 +78,37 @@ public class ChildUpdateDeleteActivity extends AppCompatActivity {
         updateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String mChildName = childName.getText().toString().trim();
+                String mDateOfBirth = childDOB.getText().toString().trim();
+                String mGender = genderSpinner.getSelectedItem().toString().trim();
+                String mGuardianName = guardianName.getText().toString().trim();
+                String mPhone = phone.getText().toString().trim();
+                String mEmail = email.getText().toString().trim();
+
+                if(TextUtils.isEmpty(mChildName)){
+                    childName.setError("Required!");
+                    return;
+                }
+                if(TextUtils.isEmpty(mDateOfBirth)){
+                    childDOB.setError("Required!");
+                    return;
+                }
+                if(TextUtils.isEmpty(mGuardianName)){
+                    guardianName.setError("Required!");
+                    return;
+                }
+                if(TextUtils.isEmpty(mPhone)){
+                    phone.setError("Required!");
+                    return;
+                }
+                if(TextUtils.isEmpty(mEmail)){
+                    email.setError("Required!");
+                    return;
+                }
+
+                Child child = new Child(mId, mChildName, mDateOfBirth, mGender, mGuardianName, mPhone, mEmail);
+                databaseReference.child(mId).setValue(child);
+                Toast.makeText(getApplicationContext(),"Data Updated",Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
@@ -63,6 +118,19 @@ public class ChildUpdateDeleteActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+
+        final Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        childDOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog(DOB_DIALOG_ID);
             }
         });
     }
@@ -76,5 +144,66 @@ public class ChildUpdateDeleteActivity extends AppCompatActivity {
         else{
             return 2;
         }
+    }
+
+
+    private DatePickerDialog.OnDateSetListener datePickerListener
+            = new DatePickerDialog.OnDateSetListener() {
+
+        // when dialog box is closed, below method will be called.
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            year = selectedYear;
+            month = selectedMonth;
+            day = selectedDay;
+
+            // set selected date into TextInputEditText childDOBEditText
+            childDOB.setText(new StringBuilder().append(day)
+                    .append("/").append(displayTheMonthInCharacters(month)).append("/").append(year)
+                    .append(" "));
+        }
+    };
+
+    //Displays a new dialog for date picker
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DOB_DIALOG_ID:
+                return new DatePickerDialog(this,
+                        datePickerListener, year, month,day);
+        }
+        return null;
+    }
+
+    //Returns the Month Abbreviation for the corresponding number.
+    private String displayTheMonthInCharacters(int passedMonth){
+        switch(passedMonth){
+            case 0:
+                return "Jan";
+            case 1:
+                return"Feb";
+            case 2:
+                return"Mar";
+            case 3:
+                return"Apr";
+            case 4:
+                return"May";
+            case 5:
+                return"Jun";
+            case 6:
+                return"Jul";
+            case 7:
+                return"Aug";
+            case 8:
+                return"Sept";
+            case 9:
+                return"Oct";
+            case 10:
+                return"Nov";
+            case 11:
+                return"Dec";
+
+        }
+        return "";
     }
 }
